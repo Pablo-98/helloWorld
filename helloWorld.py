b28797aab9649ddff6d2e4ddd5daf928068bb1c9
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-01-26 18:53:33 -0700

    print statement added

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+# in python we write comments with hashtags!
+
+
+print('Hello world');
+
+# in computng 'script' is used to refer to a file containing
+# a logical sequence  of orders or a batch processing file. 
+#this is usually a simple program.
+
+#a plain text file cointaining python code that is 
+#intended by the user is called a 'script' (an informal term that means top-level program file.)
+
+# a module is a plain text file that contains python code that is desinged to be imported and used from another python file.\
+
+#modules are meant to be imported while scripts are made to be directly executed 
+
+#to interact with code type 'python' or 'python3' depending on my installation
+
